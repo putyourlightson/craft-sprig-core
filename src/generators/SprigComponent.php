b2d0fa481ced819e281e626d25564945c2f0fba4
Ben Croker <ben@putyourlightson.com>
2023-05-10 16:10:26 -0500

    Add boilerplate methods

--- src/generators/SprigComponent.php
+++ src/generators/SprigComponent.php
@@ -11,26 +11,50 @@ use putyourlightson\sprig\base\Component;
  */
 class SprigComponent extends BaseGenerator
 {
+    private string $templatePath;
+
     public function run(): bool
     {
         $className = $this->classNamePrompt('Component name:', [
             'required' => true,
         ]);
 
+        $this->templatePath = $this->command->prompt('Template path: (optional)', [
+            'pattern' => '/^[a-z0-9_\/]*$/i',
+        ]);
+
         $namespace = (new PhpNamespace('sprig\\components'))
             ->addUse(Component::class);
 
         $class = $this->createClass($className, Component::class, [
-            self::CLASS_PROPERTIES => [
-                '_template'
-            ],
+            self::CLASS_PROPERTIES => $this->properties(),
+            self::CLASS_METHODS => $this->methods(),
         ]);
 
         $namespace->add($class);
+        $class->setComment('Sprig component');
 
         $this->writePhpClass($namespace);
         $this->command->success("**Sprig component created!**");
 
         return true;
+    }
+
+    private function methods(): array
+    {
+        return [
+            'render' => <<<PHP
+// ...
+
+return parent::render();
+PHP,
+        ];
+    }
+
+    private function properties(): array
+    {
+        return [
+            '_template' => $this->templatePath,
+        ];
     }
 }
