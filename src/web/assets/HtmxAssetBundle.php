5e16681c5ba2edbb914df4d8a3e6a366fa2f199b
Brian Hanson <brian@brianhanson.net>
2023-09-25 13:50:45 -0500

    Remove preload bundle

--- src/web/assets/HtmxAssetBundle.php
+++ src/web/assets/HtmxAssetBundle.php
@@ -9,7 +9,7 @@ class HtmxAssetBundle extends AssetBundle
 {
     public function init(): void
     {
-        $this->sourcePath = '@putyourlightson/sprig/resources/lib/htmx/1.9.5';
+        $this->sourcePath = '@putyourlightson/sprig/resources/lib/htmx/1.9.6';
 
         if (Craft::$app->getConfig()->env === 'dev') {
             $this->js = [
