c107b8a094a34b78386b61412df4db6d621c33a8
Ben Croker <ben@putyourlightson.com>
2026-07-15 11:03:56 +0200

    Ensure event listener only runs once

--- src/base/Component.php
+++ src/base/Component.php
@@ -320,7 +320,7 @@ abstract class Component extends BaseComponent implements ComponentInterface
                         htmx.trigger(component, 'refresh');
                     }
                 });
-            });
+            }, { once: true });
         JS;
 
         Craft::$app->getView()->registerJs($js, View::POS_END);
